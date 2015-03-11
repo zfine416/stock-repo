af8c637e5de19db0200dfcf9165837ee9838b59f
Zachary Fine <zfine416@gmail.com>
2015-03-11 12:52:54 -0400

    second-routes

--- stock-project/config/routes.rb
+++ stock-project/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'session#index'
+  root 'session#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
