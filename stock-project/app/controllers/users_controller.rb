22d308214e8275c520c8a4a00f0f2b9366804442
Zachary Fine <zfine416@gmail.com>
2015-03-11 12:44:56 -0400

    controllers

--- stock-project/app/controllers/users_controller.rb
+++ stock-project/app/controllers/users_controller.rb
@@ -1,2 +1,62 @@
 class UsersController < ApplicationController
+	before_action :user_is_current_user, only: [:edit, :update] 
+
+	def current
+		respond_to do |format|
+			format.json {
+				render json: {
+					id: current_user.id,
+					email: current_user.email	
+				}
+			}
+		end
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new( user_params )
+		if @user.save
+			redirect_to new_session_url
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@user = current_user
+	end
+
+	def update
+		@user = current_user
+
+		if @user.authenticate(params[:user][:current_password])
+			@user.update(password_params)
+			# redirect_to
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+
+	def password_params
+		params.require(:user).permit(:password, :password_confirmation)
+	end
+
+	def user_is_current_user
+		user = User.find(params[:id])
+
+		if user
+			# redirect_to 
+		else
+			redirect_to new_session_path
+		end
+	end
 end
