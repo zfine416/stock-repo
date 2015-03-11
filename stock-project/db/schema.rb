f9b2e4535446aac61a382bed5feb782789ef8e7a
alpha <kvnswny@gmail.com>
2015-03-11 15:19:34 -0400

    schema and gemfile added to gitignore

--- stock-project/db/schema.rb
+++ stock-project/db/schema.rb
@@ -11,30 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150311162825) do
-=======
 ActiveRecord::Schema.define(version: 20150311165208) do
->>>>>>> 9a7041258fbc154119c7e9c49b64ce5e131ab3d9
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "fav_stocks", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "stock_id"
-=======
-  create_table "stocks", force: :cascade do |t|
-    t.string   "ticker"
-    t.string   "name"
->>>>>>> 9a7041258fbc154119c7e9c49b64ce5e131ab3d9
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
-=======
+  create_table "stocks", force: :cascade do |t|
+    t.string   "ticker"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", force: :cascade do |t|
     t.string   "tweet_sender"
     t.text     "tweet_text"
@@ -43,12 +38,11 @@ ActiveRecord::Schema.define(version: 20150311165208) do
     t.datetime "updated_at",   null: false
   end
 
->>>>>>> 9a7041258fbc154119c7e9c49b64ce5e131ab3d9
   create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.string   "session_token"
+    t.string   "favorite_stocks"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
