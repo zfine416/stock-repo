a257a746e700b6737fe31da6a2d8c6fdf8467b87
Zachary Fine <zfine416@gmail.com>
2015-03-11 14:33:43 -0400

    users schema

--- stock-project/db/schema.rb
+++ stock-project/db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150311165208) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.string   "session_token"
