ee13498b95a4a7743e13d8c53b067ac62099696e
Zachary Fine <zfine416@gmail.com>
2015-03-12 12:35:39 -0400

    stock model

--- stock-project/app/models/stock.rb
+++ stock-project/app/models/stock.rb
@@ -2,9 +2,10 @@ class Stock < ActiveRecord::Base
   before_create :get_data_from_yahoo
 
   def get_data_from_yahoo
-    response = Net::HTTP.get "http://finance.yahoo.com/?token=#{asdf}&symbol=#{symbol}"
-    response_json = JSON.parse(response)
-    self.name = response_json["name"]
+    # response = Net::HTTP.get "http://finance.yahoo.com/?token=#{asdf}&symbol=#{symbol}"
+    # response_json = JSON.parse(response)
+    # self.name = response_json["name"]
+    
   end
 
   has_many :fav_stocks
