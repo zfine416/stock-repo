632fc2a1d94380c19f02e3ac80e3ca63e6761096
alpha <kvnswny@gmail.com>
2015-03-13 10:54:54 -0400

    re-added stocks_controller

---
+++ stock-project/app/controllers/stocks_controller.rb
@@ -0,0 +1,17 @@
+class StocksController < ApplicationController
+ # before_action :require_current_user#, only: :index
+ # before_action :user_is_current_user#, only: [:index, :new, :create]
+
+ def create
+   @stock = Stock.find_or_initialize_by(ticker: params[:ticker].upcase)
+   @stock.save
+   redirect_to "/stocks/#{@stock.ticker}"
+ end
+
+
+ def show
+   @stock = Stock.find_by(ticker: params[:ticker])
+   @tweets = Tweet.where("ticker" => @stock.ticker)
+   # binding.pry
+ end
+end
