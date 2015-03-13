12793108bfe6b2a0b1db72954143d739d86fa27d
alpha <kvnswny@gmail.com>
2015-03-13 10:53:13 -0400

    stock.rb

--- stock-project/app/models/stock.rb
+++ stock-project/app/models/stock.rb
@@ -34,7 +34,7 @@ class Stock < ActiveRecord::Base
     address = URI("#{baseurl}#{path}?#{query}")
     request = Net::HTTP::Get.new address.request_uri
 
-    # Set up HTTP.
+    # Set up HTTP
     http             = Net::HTTP.new address.host, address.port
     http.use_ssl     = true
     http.verify_mode = OpenSSL::SSL::VERIFY_PEER
@@ -42,7 +42,7 @@ class Stock < ActiveRecord::Base
     consumer_key ||= OAuth::Consumer.new "LXJkKuXoRJzeyQzAx0TGoCZli", "yL3P69PF41OUObm2dPVFQujVKpxeNdtqwKMSAzSyHopA1VQOu4"
     access_token ||= OAuth::Token.new "3085560635-6gwFUx4pavCbblpFtlibvO4JYdFn6G5ugZk0nPi", "4AnySTXszbZOA9t74M0tIoH3Z0rbCvcsqnYwgz9VnQTg1"
 
-    # Issue the request.
+    # Issue the request
     request.oauth! http, consumer_key, access_token
     http.start
     response = http.request request
@@ -59,16 +59,14 @@ class Stock < ActiveRecord::Base
       end
     end
 
-    # Parse and print the Tweet if the response code was 200
+    # Parse response and call cache method
     tweets = nil
     if response.code == '200' then
       tweets = JSON.parse(response.body)
       cache_tweets(tweets)
     end
     nil
-
-    # binding.pry
-
+    binding.pry
   end
 
 
