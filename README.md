276d3fa67f28504ff37198b073bdab773fdd4614
Zachary Fine <zfine416@gmail.com>
2015-07-02 15:49:17 -0400

    css

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 This is the first commit "hooray!!!"
+
+<!-- <div class="Favorited">
+<h3 class="favorited_stocks">My Favorite Stocks</h3>
+<table class="favorite_stocks">	
+	<tr><td>Stock Name</td><td>Daily Change In Price</td></tr>
+  <% @user.favorites.each do |stock| %>
+  <tr>
+    <td><%= link_to stock.name, "/stocks/"+(stock.ticker) %></a></td>
+    <td class="percent"><%= stock.change_in_percent %></a></td>
+  </tr>
+  <% end %>
+</table>
+</div> -->
--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -4,18 +4,31 @@
 // $(function(){
 // 	stockCollection = new StockCollection();
 // 	stockCollectionView = new stockCollectionView(stockCollection);
+
+var $info = $('information');
+
 $(document).ready(function () {
 	console.log("test1");
 	moveCloud();
-	
-
+	$info.css('left', '440px');
 })
+
+$(document).load(function(){
+	var halfWidth = $info.width()/2;
+	$info.css('left', '440px');
+})
+
+
+
+
 
 var moveCloud = function(){
 	var cloud = $("#cloud");
 	$(cloud).animate({"left": $( window ).width()}, 12000, function(){cloud.css("left", -350); moveCloud()});
 
 }
+
+
 
 
 
